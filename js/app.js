a1d8441eca79f3d795802c17d844080b7fbda4ce
lybhy <33481430+lybhy@users.noreply.github.com>
2018-03-11 12:05:50 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/******************************************************
+    M A I N   F U N C T I O N S   F O R   C A R D S
+ *****************************************************/
+
 //store cardElements in an Array
 const cardElements = ['fa-grunt', 'fa-android', 'fa-google',
     'fa-github', 'fa-apple', 'fa-app-store', 'fa-codepen', 'fa-css3-alt',
@@ -30,16 +34,22 @@ function levelIncrement() {
     //console.log(level);
 }
 
-//take off a specific number of cards from the fullCardArray
+//set level (for playing without tutorial)
+function setLevel(number) {
+    level = number;
+    document.querySelector('.level').innerHTML = level;
+}
 
+//take off a specific number of cards from the fullCardArray
 let reducedCardArray = [];
+let tutorial = 0;
 
 function reduceArray(array) {
-    if (level === 1) {
+    if (tutorial === 1 && level === 1) {
         for (let i = 0; i < level; i++) {
             reducedCardArray.push(array[i]);
         }
-    } else if (level < 5) {
+    } else if (tutorial === 1 && level < 5) {
         for (let i = 0; i < level * 2; i++) {
             reducedCardArray.push(array[i]);
         }
@@ -91,6 +101,10 @@ function setCurrentCards() {
     return cardArray;
 }
 
+/******************************************************
+    M O V E S   A N D   S T A R - R A T I N G
+ *****************************************************/
+
 //set move counter
 let moves = 0;
 let resultStar;
@@ -101,33 +115,137 @@ function moveCounter() {
     const star = document.querySelector('.stars');
     const stars = '<li><i class="fas fa-star"></i></li>';
     const halfstars = '<li><i class="fas fa-star-half"></i></li>';
-    if (level === 1) {
-        if (moves === 1) {
-            star.innerHTML = stars.repeat(5);
-        } else {
-            star.innerHTML = stars;
-        }
-    } else if (moves < level * 1.5) {
+    if (tutorial === 1 && level === 1) {
         star.innerHTML = stars.repeat(5);
-    } else if (moves < level * 1.7) {
+    } else if (moves < cardArray.length * 1.5) {
+        star.innerHTML = stars.repeat(5);
+    } else if (moves < cardArray.length * 1.7) {
         star.innerHTML = stars.repeat(4) + halfstars;
-    } else if (moves < level * 1.8) {
+    } else if (moves < cardArray.length * 1.8) {
         star.innerHTML = stars.repeat(4);
-    } else if (moves < level * 2) {
+    } else if (moves < cardArray.length * 2) {
         star.innerHTML = stars.repeat(3) + halfstars;
-    } else if (moves < level * 2.1) {
+    } else if (moves < cardArray.length * 2.1) {
         star.innerHTML = stars.repeat(3);
-    } else if (moves < level * 2.3) {
+    } else if (moves < cardArray.length * 2.3) {
         star.innerHTML = stars.repeat(2) + halfstars;
-    } else if (moves < level * 2.5) {
+    } else if (moves < cardArray.length * 2.5) {
         star.innerHTML = stars.repeat(2);
-    } else if (moves < level * 2.7) {
+    } else if (moves < cardArray.length * 2.7) {
         star.innerHTML = stars.repeat(1) + halfstars;
     } else {
         star.innerHTML = stars;
     }
     resultStar = star.innerHTML;
+    //return resultStar;
 };
+
+//reset moveCounter
+function resetMoveCounter() {
+    moves = 0;
+    document.querySelector('.moves').innerHTML = moves;
+    const star = document.querySelector('.stars');
+    const stars = '<li><i class="fas fa-star"></i></li>';
+    star.innerHTML = stars.repeat(5);
+}
+
+/******************************************************
+    T I M E R    F U N C T I O N S
+ *****************************************************/
+//reference for inspiration code of a Stop Watch: https://codepen.io/cathydutton/pen/GBcvo?q=javascript+stopwatch&limit=all&type=type-pens
+
+
+let minutes = 00;
+let seconds = 00;
+let appendSeconds = document.querySelector('.seconds')
+let appendMinutes = document.querySelector('.minutes')
+let buttonContinue = document.querySelector('.continue');
+let buttonPause = document.querySelector('.pause');
+let Interval;
+let timeCheck;
+let timeToWin = 0;
+
+//timing function shows the time for tutorial mode
+function startTimer() {
+    seconds++;
+    if (seconds < 9) {
+        appendSeconds.innerHTML = "0" + seconds;
+    }
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
+    }
+    if (seconds > 59) {
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+    }
+    if (minutes > 9) {
+        appendMinutes.innerHTML = minutes;
+    }
+}
+
+//set the timing function when going in the paused game mode
+buttonPause.onclick = function() {
+    clearInterval(Interval);
+    clearInterval(timeCheck);
+    document.querySelector('.pause-container').setAttribute('style', 'display: block');
+}
+
+//set the timing function for the paused game mode, when clicking continue
+buttonContinue.onclick = function() {
+    clearInterval(Interval);
+    if (tutorial === 1) {
+        Interval = setInterval(startTimer, 1000);
+    } else {
+        Interval = setInterval(stopWatch, 1000);
+    }
+    document.querySelector('.pause-container').setAttribute('style', 'display: none');
+}
+
+//set the time to win if playing arcade (no tutorial)
+function timeSet() {
+    let defaultTime = 60;
+    timeToWin = defaultTime - level;
+    seconds = timeToWin;
+    return seconds;
+}
+
+//stopwatch function shows the time if playing arcade (no tutorial)
+function stopWatch() {
+    seconds--;
+    if (seconds < 9) {
+        appendSeconds.innerHTML = "0" + seconds;
+    }
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
+    }
+    if (seconds > 59) {
+        //console.log("minutes");
+        minutes++;
+        appendMinutes.innerHTML = "0" + minutes;
+        seconds = 0;
+        appendSeconds.innerHTML = "0" + 0;
+    }
+    if (minutes > 9) {
+        appendMinutes.innerHTML = minutes;
+    }
+}
+
+//verifies the time in arcade mode: if time is out, game is lost
+function timeRunner() {
+    if (appendMinutes.innerHTML === "00" && appendSeconds.innerHTML === "00") {
+        document.querySelector('.lose').setAttribute('style', 'display: block');
+        clearInterval(timeCheck);
+    } else {
+        console.log('check');
+    }
+}
+
+
+/******************************************************
+    O P E N   &   C L O S E  C A R D S
+ *****************************************************/
 
 //open card, count click & add to openCardArray
 let t0;
@@ -137,22 +255,24 @@ let clickCounter = 0;
 
 function open() {
     clickCounter += 1;
-    if(level > 4) {
-        clearInterval(timeCheck);
-        timeCheck = setInterval(timeRunner, 1000);
-    }
-    if (clickCounter === 1 && level < 5) {
+    //start the timer for tutorial mode
+    if (clickCounter === 1 && tutorial === 1) {
         t0 = performance.now();
         clearInterval(Interval);
         Interval = setInterval(startTimer, 1000);
     }
-    if (clickCounter === 1 && level >= 5) {
+    //start the timer for arcade mode
+    else if (clickCounter === 1 && tutorial === 0) {
         t0 = performance.now();
-        timeSet();
+        //start the stopwatch
         clearInterval(Interval);
         Interval = setInterval(stopWatch, 1000);
+        //verify if game is lost
+        clearInterval(timeCheck);
+        timeCheck = setInterval(timeRunner, 1000);
     }
-    let currentCard;
+    //let currentCard;
+    //push opened card to an array, verify if card matches and check for winning
     if (openCardArray.length < 2) {
         openCardArray.push(this);
         for (let i = 0; i < openCardArray.length; i++) {
@@ -163,7 +283,7 @@ function open() {
     }
 }
 
-//close cards
+//close cards (if they don't match)
 function close() {
     for (let i = 0; i < cardArray.length; i++) {
         if (cardArray[i].classList.contains('match')) {
@@ -174,19 +294,27 @@ function close() {
     }
 }
 
+
+/******************************************************
+    M A T C H I N G    C A R D S
+ *****************************************************/
+
 //check if cards are equal
 function matching() {
-    //check if they are the same
     if (openCardArray.length === 2 && openCardArray["0"].children["0"].className === openCardArray["1"].children["0"].className) {
+        //style matched cards
         openCardArray["0"].classList.add('match', 'animated', 'tada');
         openCardArray["1"].classList.add('match', 'animated', 'tada');
-        if (level === 1) {
+        //show a "awesome" notice on tutorial
+        if (tutorial === 1 && level === 1) {
             document.querySelector('.status1').setAttribute('style', 'visibility: visible');
             document.querySelector('.status1').classList.add('animated', 'pulse');
         }
-        let random = Math.floor(Math.random() * 10);
-        if (level >= 5) {
+        //set Arcade Game Mode goodies
+        if (tutorial === 0) {
+            let random = Math.floor(Math.random() * 10);
             if (random === 2 || random === 4) {
+                //give 5 seconds more time every time a heart appears after card matched
                 statusShow();
                 setTimeout(function() {
                     statusHide();
@@ -209,14 +337,18 @@ function matching() {
                 }
             }
         }
+        //after checked they matched, empty the array
         openCardArray = [];
+        //count the move
         moveCounter();
-
+    //if they doens't match, they should be closed after enough time looking at them
     } else if (openCardArray.length === 2 && openCardArray["0"].children["0"].className != openCardArray["1"].children["0"].className) {
-        //console.log('unmatched');
+        //styles unmatched
         openCardArray["0"].classList.add('unmatched', 'animated', 'shake');
         openCardArray["1"].classList.add('unmatched', 'animated', 'shake');
+        //count the move
         moveCounter();
+        //reset cardset
         setTimeout(function() {
             openCardArray = [];
             close();
@@ -224,21 +356,16 @@ function matching() {
     }
 }
 
-function timeRunner() {
-    if (appendMinutes.innerHTML === "00" && appendSeconds.innerHTML === "00") {
-        document.querySelector('.lose').setAttribute('style', 'display: block');
-        clearInterval(timeCheck);
-    } else {
-        console.log('check');
-    }
-}
+
+/******************************************************
+    W I N N I N G    L E V E L
+ *****************************************************/
 
 function winner() {
     let matchedCards = document.querySelectorAll('.match').length;
-    if (matchedCards === cardArray.length /*&& cardArray.length < 16*/ ) {
+    if (matchedCards === cardArray.length) {
         t1 = performance.now();
         setTimeout(function() {
-            /*document.querySelector('.container').setAttribute('style', 'display: none');*/
             document.querySelector('.level-container').setAttribute('style', 'display: block');
             document.querySelector('.level-wmoves').innerHTML = moves;
             document.querySelector('.level-wstars').innerHTML = resultStar;
@@ -249,155 +376,42 @@ function winner() {
     }
 }
 
-let minutes = 00;
-let seconds = 00;
-let appendSeconds = document.querySelector('.seconds')
-let appendMinutes = document.querySelector('.minutes')
-let buttonContinue = document.querySelector('.continue');
-let buttonPause = document.querySelector('.pause');
-let Interval;
-let timeCheck;
 
-buttonContinue.onclick = function() {
-    clearInterval(Interval);
-    if (level < 5) {
-        Interval = setInterval(startTimer, 1000);
-    } else {
-        Interval = setInterval(stopWatch, 1000);
-    }
-    document.querySelector('.pause-container').setAttribute('style', 'display: none');
-}
+/******************************************************
+    C O M U N I C A T I O N S
+ *****************************************************/
 
-buttonPause.onclick = function() {
-    clearInterval(Interval);
-    clearInterval(timeCheck);
-    document.querySelector('.pause-container').setAttribute('style', 'display: block');
-}
-
-//reference for inspiration code of a Stop Watch: https://codepen.io/cathydutton/pen/GBcvo?q=javascript+stopwatch&limit=all&type=type-pens
-function startTimer() {
-    seconds++;
-    if (seconds < 9) {
-        appendSeconds.innerHTML = "0" + seconds;
-    }
-    if (seconds > 9) {
-        appendSeconds.innerHTML = seconds;
-    }
-    if (seconds > 59) {
-        minutes++;
-        appendMinutes.innerHTML = "0" + minutes;
-        seconds = 0;
-        appendSeconds.innerHTML = "0" + 0;
-    }
-    if (minutes > 9) {
-        appendMinutes.innerHTML = minutes;
-    }
-}
-//set the time to win after level 4
-let timeToWin = 0;
-
-function timeSet() {
-    let defaultTime = 60;
-    timeToWin = defaultTime - level;
-    seconds = timeToWin;
-    return seconds;
-}
-
-function stopWatch() {
-    seconds--;
-    if (seconds < 9) {
-        appendSeconds.innerHTML = "0" + seconds;
-    }
-    if (seconds > 9) {
-        appendSeconds.innerHTML = seconds;
-    }
-    if (seconds > 59) {
-        //console.log("minutes");
-        minutes++;
-        appendMinutes.innerHTML = "0" + minutes;
-        seconds = 0;
-        appendSeconds.innerHTML = "0" + 0;
-    }
-    if (minutes > 9) {
-        appendMinutes.innerHTML = minutes;
-    }
-}
-
-
-//show status
+//show status - for the random bonus of 5 seconds
 function statusShow() {
     document.querySelector('.status').setAttribute('style', 'display: block');
 }
-//hide status
+//hide status - for the random bonus of 5 seconds
 function statusHide() {
     document.querySelector('.status').setAttribute('style', 'display: none');
 }
 
-//hide first status
+//hide "awesome" notice (used in tutorial at first level)
 function status1Hide() {
     document.querySelector('.status1').setAttribute('style', 'display: none');
 }
-//hide the start message
+//hide the tutorial phrases on top of the screen
 function hideStart() {
-    if (level > 3) {
-        level
+    if (tutorial === 0) {
         document.querySelector('.start').setAttribute('style', 'display: none');
         document.querySelector('.start1').setAttribute('style', 'display: none');
     }
 }
+
+//closes the informations after the tutorial
 function closeLev5() {
     document.querySelector('.level5').setAttribute('style', 'display: none');
 }
 
 
-//new Game - one level up
-function nextLevel() {
-    clickCounter = 0;
-    status1Hide();
-    openCardArray = [];
-    cardArray = [];
-    reducedCardArray = [];
-    doubleArray = [];
-    moves = 0;
-    levelIncrement();
-    hideStart();
-    if (level < 5) {
-        clearInterval(Interval);
-        seconds = "00";
-        minutes = "00";
-        appendSeconds.innerHTML = seconds;
-        appendMinutes.innerHTML = minutes;
-    } else if (level ===5) {
-        clearInterval(Interval);
-        document.querySelector('.level5').setAttribute('style', 'display: block');
-        document.querySelector('.close-level5').addEventListener('click', closeLev5);
-    } else {
-        clearInterval(Interval);
-    }
-    document.querySelector('.container').setAttribute('style', 'display: flex');
-    document.querySelector('.level-container').setAttribute('style', 'display: none');
+/******************************************************
+    S C R E E N   S E T T I N G S
+ *****************************************************/
 
-    shuffle(cardElements);
-    reduceArray(cardElements);
-    doubled(reducedCardArray);
-    shuffle(doubleArray)
-    createDeck();
-    setCurrentCards();
-    //set click event (open card)
-    for (let i = 0; i < cardArray.length; i++) {
-        cardArray[i].addEventListener('click', open);
-    }
-}
-
-//restart all
-function restartAll() {
-    location.reload();
-}
-
-//restart current level
-function restartCurrent() {
-
-}
 
 //hide-show settings and header
 function reduced() {
@@ -412,6 +426,97 @@ function reduced() {
 }
 
 
+/******************************************************
+    S T A R T    F U N C T I O N S
+ *****************************************************/
+
+//starts Tutorial
+function tutorialPlay(){
+    tutorial = 1;
+    document.querySelector('.tutorial-container').setAttribute('style', 'display: none');
+    setLevel(0);
+    nextLevel();
+}
+
+//starts Arcade Game
+function arcadePlay() {
+    tutorial = 0;
+    hideStart();
+    status1Hide();
+    document.querySelector('.tutorial-container').setAttribute('style', 'display: none');
+    clearInterval(Interval);
+}
+
+//new Game - one level up
+function nextLevel() {
+    //set information if tutorial is ended
+    if (tutorial === 1 && level === 4) {
+        document.querySelector('.level5').setAttribute('style', 'display: block');
+        document.querySelector('.close-level5').addEventListener('click', closeLev5);
+        tutorial = 0;
+        level = 0;
+    }
+    //hide elements that aren't interesting at this point
+    document.querySelector('.level-container').setAttribute('style', 'display: none');
+    status1Hide();
+    hideStart();
+    //empty arrays
+    openCardArray = [];
+    cardArray = [];
+    reducedCardArray = [];
+    doubleArray = [];
+    //reset clickCounter
+    clickCounter = 0;
+    //reset moveCounter
+    resetMoveCounter();
+    //increment level
+    levelIncrement();
+    //set cards
+    shuffle(cardElements);
+    reduceArray(cardElements);
+    doubled(reducedCardArray);
+    shuffle(doubleArray)
+    createDeck();
+    setCurrentCards();
+    //set the goal time on Arcade Mode
+    if (tutorial === 0) {
+        timeSet();
+        if (seconds < 9) {
+            appendSeconds.innerHTML = "0" + seconds;
+        }
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
+        if (seconds > 59) {
+            //console.log("minutes");
+            minutes++;
+            appendMinutes.innerHTML = "0" + minutes;
+            seconds = 0;
+            appendSeconds.innerHTML = "0" + 0;
+        }
+        if (minutes > 9) {
+            appendMinutes.innerHTML = minutes;
+        }
+    }
+    //set click event (open card)
+    for (let i = 0; i < cardArray.length; i++) {
+        cardArray[i].addEventListener('click', open);
+    }
+    //show deck with cards
+    document.querySelector('.container').setAttribute('style', 'display: flex');
+}
+
+//restart all
+function restartAll() {
+    location.reload();
+}
+
+//restart current level
+function restartCurrent() {
+
+}
+
+//global start event listeners and function calls
 document.addEventListener('DOMContentLoaded', function() {
     levelIncrement();
     shuffle(cardElements);
@@ -434,4 +539,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let j = 0; j < cardArray.length; j++) {
         cardArray[j].addEventListener('click', open);
     }
+    document.querySelector('.tutorial-container').setAttribute('style', 'display: block');
+    document.querySelector('.tutorial').addEventListener('click', tutorialPlay);
+    document.querySelector('.play-direct').addEventListener('click', arcadePlay);
 });
